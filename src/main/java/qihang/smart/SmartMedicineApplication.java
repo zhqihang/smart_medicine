defbd2ae671816cca590a0b282d02ad4a0be18f9
zhqihang <qihang9903@163.com>
2024-07-15 19:53:33 +0800

    项目初始化

---
+++ src/main/java/qihang/smart/SmartMedicineApplication.java
@@ -0,0 +1,13 @@
+package qihang.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMedicineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qihang/smart/SmartMedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package qihang.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMedicineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
