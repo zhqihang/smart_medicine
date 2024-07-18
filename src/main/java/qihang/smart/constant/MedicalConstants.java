ee50202bbf229633422547cdb67b7d4cc73ff3ab
zhqihang <qihang9903@163.com>
2024-07-18 12:55:06 +0800

    新增系统跳转模块

---
+++ src/main/java/qihang/smart/constant/MedicalConstants.java
@@ -0,0 +1,25 @@
+package qihang.smart.constant;
+
+/**
+ * @description: 疾病常量类
+ * @author: zhqihang
+ * @date: 2024/07/17
+ */
+public class MedicalConstants {
+
+    /**
+     * 用户操作类型：搜索
+     */
+    public static final Integer TYPE_OPERATE = 1;
+
+    /**
+     * 用户操作类型：查看某种疾病
+     */
+    public static final Integer TYPE_ILLNESS = 2;
+
+    /**
+     * 用户操作类型：查看相关的药
+     */
+    public static final Integer TYPE_MEDICINE = 3;
+
+}
